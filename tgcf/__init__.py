3ecf7e8b5f85eb8482583f8495a70158dd16e703
Sh-Jil <65016071+Sh-Jil@users.noreply.github.com>
2021-07-04 12:51:01 +0530

    Add files via upload

---
+++ tgcf/__init__.py
@@ -0,0 +1,9 @@
+"""Package tgcf.
+
+The ultimate tool to automate custom telegram message forwarding.
+https://github.com/Sh-Jil/tgautomater
+"""
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
